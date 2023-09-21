e67d29543e27fa53a0cc2c45cfd0d39321177ea9
Sharmin64 <lizaeatsbanana@gmail.com>
2023-09-21 21:37:02 +0600

    standardMessageForm added to the component

--- src/components/customMessageForms/StandardMessageForm.jsx
+++ src/components/customMessageForms/StandardMessageForm.jsx
@@ -1,7 +1,48 @@
-import React from "react";
+import React, {useState} from "react";
+import {XMarkIcon} from "@heroicons/react/24/solid";
+import Dropzone from "react-dropzone";
 
 const StandardMessageForm = () => {
-  return <div>StandardMessageForm</div>;
+  const [message, setMessage] = useState("");
+  const [attachment, setAttachment] = useState("");
+  const [preview, setPreview] = useState("");
+
+  const handleChange = (e) => setMessage(e.target.value);
+  return (
+    <div className="message-form-container">
+      {preview && (
+        <div className="message-form-preview">
+          <img
+            className="message-form-preview-image"
+            src={preview}
+            alt="message-form-preview"
+            onLoad={() => URL.revokeObjectURL(preview)}
+          />
+          <XMarkIcon
+            className="message-form-icon-x"
+            onClick={() => {
+              setPreview("");
+              setAttachment("");
+            }}
+          />
+        </div>
+      )}
+      <div className="message-form">
+        <div className="message-form-input-container">
+          <input
+            type="text"
+            className="message-form-input"
+            value={message}
+            onChange={handleChange}
+            placeholder="Send a message..."
+          />
+        </div>
+        <div className="message-form-icons">
+          <Dropzone acceptedFiles=".jpg, .jpeg, .png" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default StandardMessageForm;
