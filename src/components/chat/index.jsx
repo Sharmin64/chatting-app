dd470b68e9b1320e2121d4c6785819015c677eeb
Sharmin64 <lizaeatsbanana@gmail.com>
2023-09-29 09:13:17 +0600

    login component created

--- src/components/chat/index.jsx
+++ src/components/chat/index.jsx
@@ -7,11 +7,13 @@ import {
 import Header from "@/components/customHeader";
 import StandardMessageForm from "@/components/customMessageForms/StandardMessageForm";
 import Ai from "@/components/customMessageForms/Ai";
-const Chat = () => {
+import AiCode from "@/components/customMessageForms/AiCode";
+import AiAssist from "@/components/customMessageForms/AiAssist";
+const Chat = ({user, secret}) => {
   const chatProps = useMultiChatLogic(
     import.meta.env.VITE_PROJECT_ID,
-    "lizukha",
-    "2345"
+    user,
+    secret
   );
 
   return (
@@ -24,6 +26,12 @@ const Chat = () => {
         renderMessageForm={(props) => {
           if (chatProps.chat?.title.startsWith("AiChat_")) {
             return <Ai props={props} activeChat={chatProps.chat} />;
+          }
+          if (chatProps.chat?.title.startsWith("AiCode_")) {
+            return <AiCode props={props} activeChat={chatProps.chat} />;
+          }
+          if (chatProps.chat?.title.startsWith("AiAssist_")) {
+            return <AiAssist props={props} activeChat={chatProps.chat} />;
           }
           return (
             <StandardMessageForm props={props} activeChat={chatProps.chat} />
